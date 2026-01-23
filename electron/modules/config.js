7136e04966ed4975ad60d6d29d50af4434525298
tago <78416380+t-a-g-o@users.noreply.github.com>
2026-01-22 21:41:16 -0800

    chore: bump version to 9.8.6

--- electron/modules/config.js
+++ electron/modules/config.js
@@ -9,7 +9,7 @@ const { app } = require("electron");
 
 // Current app version
 // Do not change this value
-const appVersion = "9.7.6";
+const appVersion = "9.8.6";
 
 // Environment detection
 const isDev = !app.isPackaged;
