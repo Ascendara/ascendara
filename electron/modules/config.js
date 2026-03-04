347e632c523915fb10216e7f9bc7fc5328e380f5
tago <itslocaltago@gmail.com>
2026-03-04 14:04:17 -0800

    chore: bump version to 10.2.4

--- electron/modules/config.js
+++ electron/modules/config.js
@@ -136,7 +136,7 @@ const DEPENDENCY_REGISTRY_PATHS = {
 // ============================================================================
 
 const appBranch = "live";
-const appVersion = "10.2.3";
+const appVersion = "10.2.4";
 const testingVersion = "";
 
 module.exports = {
