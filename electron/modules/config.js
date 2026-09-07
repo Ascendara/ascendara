f7951cd036215e991b54701b4ba0a1012d2de9bf
tago <78416380+t-a-g-o@users.noreply.github.com>
2026-09-07 12:18:16 -0700

    chore: bump version to 10.8.4

--- electron/modules/config.js
+++ electron/modules/config.js
@@ -140,7 +140,7 @@ const DEPENDENCY_REGISTRY_PATHS = {
 // ============================================================================
 
 const appBranch = "live";
-const appVersion = "10.8.3";
+const appVersion = "10.8.4";
 const testingVersion = "";
 
 module.exports = {
